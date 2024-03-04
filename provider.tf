b5e7a708fdbd58d73c54d4f49333585a078cb789
NareshPonna20 <159776551+NareshPonna20@users.noreply.github.com>
2024-03-04 19:24:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,3 +10,13 @@ terraform {
 provider "azurerm" {
   # Configuration options
 }
+
+terraform {
+ backend "azurerm" {
+  storage_name  = "terraformcicdsa"
+  resource_group_name = "terraformRG"
+  container_name = "cicdcontainer"
+  key            = "workflowstate"
+  SAS_Token      =  "sp=ral&st=2024-03-04T13:53:53Z&se=2024-03-04T21:53:53Z&spr=https&sv=2022-11-02&sr=c&sig=%2FbCZlAM2Wl1Z4UlXvr1WmhXebrpqW1VhqsYGUee8Vm4%3D"
+ }
+}
