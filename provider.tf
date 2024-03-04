e24708951f594bae07a822fa82272bd1111bd462
NareshPonna20 <159776551+NareshPonna20@users.noreply.github.com>
2024-03-04 19:30:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,11 +12,14 @@ provider "azurerm" {
 }
 
 terraform {
- backend "azurerm" {
-  storage_name  = "terraformcicdsa"
-  resource_group_name = "terraformRG"
-  container_name = "cicdcontainer"
-  key            = "workflowstate"
-  SAS_Token      =  "sp=ral&st=2024-03-04T13:53:53Z&se=2024-03-04T21:53:53Z&spr=https&sv=2022-11-02&sr=c&sig=%2FbCZlAM2Wl1Z4UlXvr1WmhXebrpqW1VhqsYGUee8Vm4%3D"
- }
+  backend "azurerm" {
+    storage_account_name = "terraformcicdsa"
+    container_name       = "cicdcontainer"
+    key                  = "prod.terraform.tfstate"
+
+    # rather than defining this inline, the SAS Token can also be sourced
+    # from an Environment Variable - more information is available below.
+    sas_token = "sp=racwl&st=2024-03-04T14:00:04Z&se=2025-03-04T22:00:04Z&spr=https&sv=2022-11-02&sr=c&sig=KZEY0FLz5e2tor8CgiNk6g6VL3imkUvUIMWGbDSoYzw%3D"
+  }
 }
+
