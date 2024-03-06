14fa2bd363681e7c7c5dc9795fbad74f9ac1d085
NareshPonna20 <159776551+NareshPonna20@users.noreply.github.com>
2024-03-06 20:02:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,14 +6,7 @@ variable "location" {
   type = string
 }
 
-variable "subnetdetails" {
- type = map (string)
- default = {
- subnet1  = "10.0.1.0/24"
- subnet2  = "10.0.2.0/24"
- subnet3  = "10.0.3.0/24"
- }
-}
+
 
 variable "nsgdetails" {
  type  = map (string)
