9a145cdc08651d9b060029b6032f214148f1c2d0
NareshPonna20 <159776551+NareshPonna20@users.noreply.github.com>
2024-03-06 19:34:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "azurerm_resource_group" "RG" {
 }
 
 resource "azurerm_virtual_network" "VNET" {
-Name   = "VNET1010"
+virtual_network_name   = "VNET1010"
 resource_group_name = azurerm_resource_group.RG.name
 location = azurerm_resource_group.RG.location
-address_spaces = "10.0.0.0/24"
+address_space = "10.0.0.0/24"
 }
 
